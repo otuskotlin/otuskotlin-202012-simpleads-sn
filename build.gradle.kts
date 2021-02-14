3efbf3ecf0f0bd92853c1ffcb579572aa84e3fe9
Сопилов Николай Васильевич <Nikolay.Sopilov@rosbank.ru>
2021-02-14 04:17:31 +0300

    add module common-backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
+plugins {
+    kotlin("jvm") apply false
+}
+
 group = "ru.otus.otuskotlin.simpleads"
 version = "0.0.1"
 
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
 rootProject.name = "otuskotlin-simpleads"
 
+include("common-backend")
